796902afcbd8a96f9bc47b39663f15e10acd887d
hc-bradley <17600829+hc-bradley@users.noreply.github.com>
2021-11-06 21:03:47 +0000

    Create hook.js

---
+++ config/hook.js
@@ -0,0 +1,12 @@
+  module.exports = {
+    settings: {
+      // ...
+      algolia: {
+        enabled: true,
+        applicationId: 'Y9UK5I9HH8',
+        apiKey: '2660ee74b9523bdf08c69ea0c27dbc55',
+        debug: true,              // default: false
+        prefix: 'concord_prefix',  // default: Strapi environment (strapi.config.environment)
+      },
+    }
+  };
